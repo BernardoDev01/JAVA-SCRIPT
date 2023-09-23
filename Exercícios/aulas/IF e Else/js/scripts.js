2d58a963aaeb00c8637b61869569c0551b78f164
BernardoValle <dev.bernardovalle@gmail.com>
2023-09-23 08:35:11 -0300

    Teste de else e if

---
+++ Exercícios/aulas/IF e Else/js/scripts.js
@@ -0,0 +1,28 @@
+var nome = "Bernardo";
+
+if(nome == "Pedro") {
+    console.log("O nome dele é Pedro");
+} else if(nome == "Matheus"){
+    console.log("O nome é Matheus")
+} else if (nome=="Bernardo"){
+    console.log("O nome é Bernardo")
+} else if (nome == "Bruno"){
+    console.log("O nome é Bruno")
+} else { 
+    console.log("Ele possui outro nome!");
+}
+
+var idade = 15;
+if (idade >= 19) {
+    console.log("Ele pode entrar na festa!");
+} else if (idade == 18 ) {
+    console.log("Ele pode entrar na festa com acompanhante!")
+} else {
+    console.log("Ele nao pode entrar na festa")
+}
+
+if (nome == "Bernardo") {
+    console.log("Deu certo!")
+} else {
+    console.log("Tente novamente")
+} 
